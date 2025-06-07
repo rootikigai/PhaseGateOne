0483e84e4c2d846234513954b98c270550839fb5
rootikigai <ifykel360@gmail.com>
2025-06-07 09:40:10 +0100

    Writing codes for a Menstrual Calculator program. I'm a bit stuck here and wondering why if and else isn't working for this part I am commiting...mods ongoing

---
+++ Java/MenstrualCalcFunctions.java
@@ -0,0 +1,14 @@
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class MenstrualCalcFunctions{
+	public static LocalDate getAnyDateInput(String promptUser){
+		Scanner input = new Scanner(System.in);
+
+		while(true){
+      System.out.print(promptUser);
+      String userInput = scanner.nextLine();
+      if userInput
+		}
+	}
+}
