f7d538fcc61a0bf2b742f0fe11e70bd090957bc5
rootikigai <ifykel360@gmail.com>
2025-06-07 12:28:29 +0100

    I call all the functions in MenstrualCalcFunctions.java here to have a complete console-based program...mods ongoing

---
+++ Java/MenstrualCalcMain.java
@@ -0,0 +1,12 @@
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class MenstrualCalcMain{
+  public static void main(String[] args){
+    Scanner input = new Scanner(System.in);
+    String message = """
+===Menstrual Calculator===
+""";
+    System.out.println(message);
+  }
+}
