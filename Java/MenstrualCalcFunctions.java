cbb97f4a641628340871655d16104c1f3b567a23
rootikigai <ifykel360@gmail.com>
2025-06-07 10:31:30 +0100

    Update on if...else problem. Try and Catch block solved it...equivalent of Try...Except in pyhton. Don't mind my exception variable :)

--- Java/MenstrualCalcFunctions.java
+++ Java/MenstrualCalcFunctions.java
@@ -8,7 +8,22 @@ public class MenstrualCalcFunctions{
 		while(true){
       System.out.print(promptUser);
       String userInput = scanner.nextLine();
-      if userInput
+      try{
+        LocalDate userDate = LocalDate.parse(userInput);
+        return userDate;
+      }
+      catch (DateTimeParseException wahala){
+        System.out.println("Invalid Date. See example: 2025-10-27, year first, month next, day last...no whine me.");
+      }
 		}
 	}
+
+  public static localDate getNextPeriodDate(LocalDate lastPeriodStartDate, int aveCycleDays){
+
+  }
+
+
+
+
+
 }
