cf6a104c2487b1bf075b11dbb0140937a45d7679
rootikigai <ifykel360@gmail.com>
2025-06-07 12:16:51 +0100

    Functions written for Date input, Next Period Date, Ovulation Date, Fertility Period and Safe Days

--- Java/MenstrualCalcFunctions.java
+++ Java/MenstrualCalcFunctions.java
@@ -1,5 +1,6 @@
 import java.time.LocalDate;
 import java.util.Scanner;
+import java.time.format.DateTimeParseException;
 
 public class MenstrualCalcFunctions{
 	public static LocalDate getAnyDateInput(String promptUser){
@@ -7,7 +8,7 @@ public class MenstrualCalcFunctions{
 
 		while(true){
       System.out.print(promptUser);
-      String userInput = scanner.nextLine();
+      String userInput = input.nextLine();
       try{
         LocalDate userDate = LocalDate.parse(userInput);
         return userDate;
@@ -18,12 +19,32 @@ public class MenstrualCalcFunctions{
 		}
 	}
 
-  public static localDate getNextPeriodDate(LocalDate lastPeriodStartDate, int aveCycleDays){
-
+  public static LocalDate getNextPeriodDate(LocalDate lastPeriodStartDate, int aveCycleDays){
+    LocalDate nextPeriodDate = lastPeriodStartDate.plusDays(aveCycleDays);
+    return nextPeriodDate;
   }
 
+  public static LocalDate getOvulationDate(LocalDate lastPeriodStartDate, int aveCycleDays){
+    LocalDate ovulationDate = lastPeriodStartDate.plusDays(aveCycleDays - 14);
+    return ovulationDate;
+  }
 
+  public static LocalDate[] getFertilityPeriod(LocalDate ovulationDate){
+    LocalDate startFertilityPeriod = ovulationDate.minusDays(2);
+    LocalDate endFertilityPeriod = ovulationDate.plusDays(2);
+    return new LocalDate[]{startFertilityPeriod, endFertilityPeriod};
+  }
 
+  public static LocalDate[] getSafeDaysPeriod(LocalDate lastPeriodStartDate, int periodDays, LocalDate[] fertilityPeriod, LocalDate nextPeriodDate){
+    LocalDate periodEnd = lastPeriodStartDate.plusDays(periodDays);
+    LocalDate safeBeforeFertStart = periodEnd;
+    LocalDate safeBeforeFertEnd = fertilityPeriod[0].minusDays(1);
 
+    LocalDate safeAfterFertEnd = fertilityPeriod[1].plusDays(1);
+    LocalDate safeBeforeNextPeriod = nextPeriodDate.minusDays(1);
 
+    return new LocalDate[]{
+      safeBeforeFertStart, safeBeforeFertEnd, safeAfterFertEnd, safeBeforeNextPeriod
+    };
+  }
 }
