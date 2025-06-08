50fa0e3c3d238fdf468be37a72a324a15b93b9b2
rootikigai <ifykel360@gmail.com>
2025-06-08 17:57:47 +0100

    Functions written using divide and conquer approach...mods ongoing

--- JavaScript/CheckoutSystem.js
+++ JavaScript/CheckoutSystem.js
@@ -11,5 +11,40 @@ function addToCart(item, qty, pricePerUnit){
     return cart.push({item, qty, pricePerUnit});
 }
 
-let customerName = prompt("Enter customer name: ");
-let customerPhone = prompt("Enter customer phone: ")
+function getCartTotal(cart){
+    let cartTotal = 0;
+    for(let item of cart){
+        cartTotal += item.qty * item.pricePerUnit;
+    }
+    return cartTotal;
+}
+
+function giveDiscount(cartTotal, discountRate){
+    let discount = cartTotal * (discountRate / 100);
+    let amountAfterDiscount = cartTotal - discount;
+    return {discount,
+            amountAfterDiscount};
+}
+
+function takeVAT(amountAfterDiscount, vatRate){
+    let vat = amountAfterDiscount * (vatRate / 100);
+    let amountAfterVAT = amountAfterDiscount + vat
+    return {vat, amountAfterVAT};
+}
+/*
+function netAmount(cartTotal, discount, vat){
+
+}
+
+function getInvoice(customer, cart, discountRate, vatRate){
+
+}
+
+function getReceipt(invoice, amountPaid){
+
+}
+
+function getBalance(amountPaid, totalAmount){
+
+}
+*/
