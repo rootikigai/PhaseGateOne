28d245cb6f4dbb426207144745d56ae1cb7d8e75
rootikigai <ifykel360@gmail.com>
2025-06-10 06:40:55 +0100

    While testing the function: test_that_give_discount_function_works I found out I could test two (or multiple) components of a single function at once...hmmm...tests ongoing

--- Python/checkout_system_test.py
+++ Python/checkout_system_test.py
@@ -1,4 +1,4 @@
-from checkout_system import get_item_price
+from checkout_system import get_item_price, get_cart_total
 from unittest import TestCase
 
 class TestCheckoutSystemFunctions(TestCase):
@@ -7,4 +7,20 @@ class TestCheckoutSystemFunctions(TestCase):
         expected = 1000
         self.assertEqual(actual, expected)
 
+    def test_that_get_cart_total_function_works(self):
+        cart = [{"item": "Bread", "qty": 2, "price_per_unit": 500}, {"item": "Milk", "qty": 1, "price_per_unit": 800}]
+        actual = get_cart_total(cart)
+        expected = (2 * 500) + (1 * 800)
+        self.assertEqual(actual, expected)
 
+    def test_that_give_discount_function_works(self):
+        cart = [{"item": "Bread", "qty": 2, "price_per_unit": 500}, {"item": "Milk", "qty": 1, "price_per_unit": 800}]
+        discount_rate = (7.5 / 100)
+        actual = get_cart_total(cart) * discount_rate
+        expected = 1800 * (7.5 / 100)
+        self.assertEqual(actual, expected)
+        discount = 1800 * (7.5 / 100)
+        amount_after_discount = get_cart_total(cart) - discount
+        actual = amount_after_discount
+        expected = 1665
+        self.assertEqual(actual, expected)
