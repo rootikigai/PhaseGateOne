942d0e4c6364768590f7e81305d1d8cd72459182
rootikigai <ifykel360@gmail.com>
2025-06-13 02:26:30 +0100

    This code will look messy. But, we will get it to work and then refactor later. Function for user choice has been written to be reused...mods ongoing

--- JavaScript/MBTI.js
+++ JavaScript/MBTI.js
@@ -1,2 +1,45 @@
 const prompt = require("prompt-sync")();
-questions [{1. }]
+let choice;
+let countA = 0;
+let countB = 0;
+let countE = 0;
+let countI = 0;
+let countS = 0;
+let countN = 0;
+let countT = 0;
+let countF = 0;
+let countJ = 0;
+let countP = 0;
+let nav;
+
+function answerChoice(){
+    while(choice !== "a" || choice !== "b"){
+        let choice = prompt("Your answer: ").toLowerCase();
+        if(choice === "a"){
+            countA += 1
+            break;
+        }
+        else if(choice === "b"){
+            countB += 1
+            break;
+        }
+        else{
+            console.log("Expected A or B as Response. I know this is an error, Please try again");
+        }
+    }
+}
+
+
+//...
+let name = prompt("What is your name?: ")
+console.log(`\nWelcome ${name}. Search yourself and think before answering. Can you say you prefer either A or B for the following questions?`)
+let question1 = console.log("A. Expend Energy, Enjoy Groups\nB. Conserve Energy, Enjoy One-on-One: ")
+answerChoice(choice);
+if (countA == 1){
+    countE++
+}
+else{
+    countI++
+}
+//console.log(countA, countB, countE, countI);
+
