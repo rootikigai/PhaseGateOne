51468fa470c995eab52f2926c241b9f6a7a4e290
rootikigai <ifykel360@gmail.com>
2025-06-13 22:22:23 +0100

    A console-based student grading management system. Initial commit.

---
+++ Java/LagbajaStudentGradeFunctions.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class LagbajaStudentGradeFunctions{
+  public static void main(String[] args){
+    Scanner input = new Scanner(System.in);
+    int numOfStudents;
+    int numOfSubjects;
+  }
+}
