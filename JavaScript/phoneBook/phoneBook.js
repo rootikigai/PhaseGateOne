ff863f1b603c25cc38b9ef692f764d9486dae6a9
rootikigai <ifykel360@gmail.com>
2025-06-15 14:06:59 +0100

    Update: Function to add contact done, an array of objects was used. 'Create' under CRUD done

---
+++ JavaScript/phoneBook/phoneBook.js
@@ -0,0 +1,15 @@
+let contacts = [];
+
+function addContact(firstName, lastName, phone){
+    let contact = {
+        firstName: firstName,
+        lastName: lastName,
+        phone: phone
+    };
+    contacts.push(contact)
+}
+/*
+addContact("Kelvin", "Ifeanyi", "08035859351");
+addContact("Root", "Ikigai", "08044554433")
+console.log(contacts)
+*/
