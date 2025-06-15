133292e2cd11820d1fe39d76894f3e019cd6808c
rootikigai <ifykel360@gmail.com>
2025-06-15 14:47:01 +0100

    Update: Function to remove contact, added. Delete in CRUD done...mods ongoing

--- JavaScript/phoneBook/phoneBook.js
+++ JavaScript/phoneBook/phoneBook.js
@@ -1,15 +1,32 @@
 let contacts = [];
+let findContact = [];
 
 function addContact(firstName, lastName, phone){
-    let contact = {
+    contact = {
         firstName: firstName,
         lastName: lastName,
         phone: phone
     };
     contacts.push(contact)
 }
+
+function removeContact(search){
+    for(let i = 0; i < contacts.length; i++){
+        let contact = contacts[i];
+        if(contact.firstName === search || contact.lastName === search || contact.phone === search){
+            contacts.splice(i, 1);
+            console.log(`Removed ${contact.firstName} ${contact.lastName}`);
+            return;
+        }
+    }
+    console.log("Not in phonebook.")
+}
 /*
 addContact("Kelvin", "Ifeanyi", "08035859351");
 addContact("Root", "Ikigai", "08044554433")
+removeContact("Kelvin");
 console.log(contacts)
 */
+
+
+
