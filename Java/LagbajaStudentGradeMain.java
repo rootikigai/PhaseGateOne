0ca8dfd2e94114a3f88c498e5f3ab8e83da59a00
rootikigai <ifykel360@gmail.com>
2025-06-17 05:12:39 +0100

    Console based school grading program

---
+++ Java/LagbajaStudentGradeMain.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class LagbajaStudentGradeMain{
+  public static void main(String[] args){
+    Scanner input = new Scanner(System.in);
+    int numOfStudents = 0;
+    int numOfSubjects = 0;
+    int[][] studentScores = new int[numOfStudents][numOfSubjects];
+    int[] overallTotalScores = new int[numOfStudents];
+    int[] totalPerSub = new int[numOfSubjects];
+    int[] totalPasses = new int[numOfSubjects];
+    int[] totalFails = new int[numOfSubjects];
+    System.out.print("How many students do you have?");
+    numOfStudents = input.nextInt();
+    System.out.print("How many subjects do they offer?");
+    numOfSubjects = input.nextInt();
+    System.out.print("Saving >>>>>>>>>>>>>>>>>>");
+    System.out.print("Saved Successfully");
+    for(int student = 0; student < numOfStudents; student++){
+      System.out.println("Entering score for student " + (student + 1));
+      for(int subject = 0; subject < numOfSubjects; subject++){
+        System.out.println("Enter score for subject " + (subject + 1) + "\nSaving >>>>>>>>>>>>>>>>\nSaved successfully");
+      }
+    }
+    
+    
+  }
+}
