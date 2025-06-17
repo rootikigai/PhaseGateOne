7a78f4e7dc33845e6bb69aac7e320a46b61489c8
rootikigai <ifykel360@gmail.com>
2025-06-17 17:24:58 +0100

    Test of knowledge in Sequence, Slection and Iteration

---
+++ FireDrillOne/Python/task_five.py
@@ -0,0 +1,4 @@
+for i in range(1, 11, 1):
+    for j in range(0, 5, 1):
+        if i % 4 == 0:
+	        print(f"{i}",end=" ")
---
+++ FireDrillOne/Python/task_four.py
@@ -0,0 +1,3 @@
+for i in range(1, 11, 1):
+    if i % 4 == 0:
+	    print(f"{i}",end=" ")
