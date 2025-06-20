baca68036a60a2f6153d85d205f332c1355f4330
rootikigai <ifykel360@gmail.com>
2025-06-20 23:48:09 +0100

    Personal completions of tasks

--- FireDrillOne/Java/TaskTwo.java
+++ FireDrillOne/Java/TaskTwo.java
@@ -2,7 +2,7 @@ public class TaskTwo{
 	public static void main(String[] args){
 		for(int i = 2; i <= 10; i++){
       if(i % 2 == 0){
-			  System.out.print(i + " ");
+			  System.out.print(i + ", ");
       }
 		}
 	}
