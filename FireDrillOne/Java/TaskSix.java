87df9869008a69e63f77669cb14b38d77d68cc3a
rootikigai <ifykel360@gmail.com>
2025-06-22 05:19:38 +0100

    Personal corrections for an incomplete task

--- FireDrillOne/Java/TaskSix.java
+++ FireDrillOne/Java/TaskSix.java
@@ -1,10 +1,29 @@
+/*
+
 public class TaskSix{
 	public static void main(String[] args){
-    int temp = 4;
+		int temp = 4;
+		for(int i = 1; i <= 10; i++){
+			if(i % 4 == 0){
+        System.out.print(i + " ");
+        int multiple = i;
+      	for(int j = 1; j < 5; j++){
+        	multiple = multiple * temp;
+          System.out.print(multiple + " ");
+      	}
+      }
+		}
+	}
+}
+
+*/
+public class TaskSix{
+	public static void main(String[] args){
 		for(int i = 1; i <= 10; i++){
       if(i % 4 == 0){
-        for(int j = 0; j < 5; j++){          
-            System.out.print((i * temp));
+        //System.out.print((i + " "));
+        for(int j = 1; j < 6; j++){
+          System.out.print(i * j + " ");
         }
       }
 		}
